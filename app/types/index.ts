fbc2cf845caf479b2e7b6e10b65bef92a6ddb73b
badhon <minhajul.arifin.badhon@gmail.com>
2025-03-16 22:47:03 -0600

    1. Category related schema and types

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,16 +1,64 @@
-export interface Subcategory {
-	id: string;
-	name: string;
-}
+import {
+	createCategorySchema,
+	createSubcategorySchema,
+	deleteCategorySchema,
+	deleteSubcategorySchema,
+	updateCategorySchema,
+	updateSubcategorySchema,
+} from "@/lib/validation";
+import { Prisma, CategoryGroup } from "@prisma/client";
+import { z } from "zod";
 
-export interface Category {
-	id: string;
-	name: string;
-	subcategories: Subcategory[];
-}
+export type BaseResponse = {
+	success: boolean;
+	statusCode: number;
+};
 
-export interface CategoryGroup {
-	id: string;
+export type SuccessResponse<T> = BaseResponse & {
+	data: T;
+};
+
+export type ErrorResponse = BaseResponse & {
+	data: string;
+};
+
+export type Response<T> = SuccessResponse<T> | ErrorResponse;
+
+export type CategoriesWithSub = Prisma.CategoryGetPayload<{
+	select: {
+		id: true;
+		name: true;
+		group: true;
+		subcategories: {
+			select: {
+				id: true;
+				name: true;
+			};
+		};
+	};
+}>;
+
+export type GroupedCategories = Partial<
+	Record<CategoryGroup, CategoriesWithSub[]>
+>;
+
+export type CreateCategoryProps = z.infer<typeof createCategorySchema>;
+export type UpdateCategoryProps = z.infer<typeof updateCategorySchema>;
+export type DeleteCategoryProps = z.infer<typeof deleteCategorySchema>;
+
+export type CreateSubcategoryProps = z.infer<typeof createSubcategorySchema>;
+export type UpdateSubcategoryProps = z.infer<typeof updateSubcategorySchema>;
+export type DeleteSubcategoryProps = z.infer<typeof deleteSubcategorySchema>;
+
+export type CategoryMutationProps = {
+	type: "category" | "subcategory";
+	operation: "new" | "edit" | "delete";
 	name: string;
-	categories: Category[];
-}
+	group: CategoryGroup;
+	categoryId: number;
+	subcategoryId?: number;
+};
+
+export type CategoryDialogProps = CategoryMutationProps & {
+	isOpen: boolean;
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export const delay = (ms: number): Promise<void> =>
+	new Promise((resolve) => setTimeout(resolve, ms));
