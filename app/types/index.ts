c4acef93d0a425d0b87614649b5ed16cb7bdb678
badhon <minhajul.arifin.badhon@gmail.com>
2026-01-10 17:03:58 -0700

    1. import fix 2. eslint fix

--- app/types/index.ts
+++ app/types/index.ts
@@ -5,6 +5,7 @@ import {
 	createMerchantSchema,
 	createSubcategorySchema,
 	createTransactionSchema,
+	deleteAllTransactionsSchema,
 	deleteCategorySchema,
 	deleteMappingSchema,
 	deleteMerchantSchema,
